a525852304c45c17d94655d2a99a1ec1b3b87ba7
Karl-Heinz <kh@krachenfels>
2022-12-02 18:36:20 +0100

    door2

---
+++ 2/mathekal_2.py
@@ -0,0 +1,26 @@
+
+def solution(a,b,c,d):
+    archy = 3*a+b+d
+    if archy!=125:
+        return False
+    bowy = 2*b+c+2*d
+    if bowy!=230:
+        return False
+    curvy= a+b+2*c+d
+    if curvy!=185:
+        return False
+    return True
+    
+
+solutions=0
+max_points=105
+for a in range(5,max_points,5):
+    for b in range(a+5,max_points,5):
+        for c in range(b+5,max_points,5):
+            for d in range (c+5,max_points,5):
+                if solution(a,b,c,d):
+                    print(f"a={a}, b={b}, c={c}, d={d}")
+                    solutions+=1
+
+print(f"Es git {solutions} Möglichkeiten")
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Mathekalender Klasse 10+ 2022
+
+## Aufgabe 1
+mit Blatt Papier gelöst
+
+## Aufgabe 2
+Brute force durchprobiert in python.
+
+##
